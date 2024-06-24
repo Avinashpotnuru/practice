923e35c1c2fd49028aaab68eb7acfbab195531a3
Avinashpotnuru <potnuruaavinash111@example.com>
2024-06-24 12:19:25 +0530

    edit

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -6,13 +6,14 @@ import TodoItem from './TodoItem'
 export interface TodoListProps{
     todoList:TodoProps[],
     deleteItem:(id:number)=>void;
-    toggleTodo:(id:number)=>void
+    toggleTodo:(id:number)=>void;
+    setTodos: React.Dispatch<React.SetStateAction<TodoProps[]>>
 }
 
-const TodoList = ({todoList,deleteItem,toggleTodo}:TodoListProps) => {
+const TodoList = ({todoList,deleteItem,toggleTodo,setTodos}:TodoListProps) => {
   return (
     <div className='flex flex-col justify-center items-center gap-4 mt-4'>
-        {todoList.map(item=><TodoItem key={item.id}  todoItem={item} deleteItem={deleteItem}  toggleTodo={toggleTodo}/>)}
+        {todoList.map(item=><TodoItem key={item.id}  todoItem={item} deleteItem={deleteItem}  toggleTodo={toggleTodo} setTodos={setTodos} todoList={todoList}/>)}
     </div>
   )
 }
